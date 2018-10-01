2df1e7ba59bdb42193d55e1baa56e64aa8281975
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-10-01 08:29:10 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,25 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<table align="center">
+		<form action="prosesLogin.php" method="POST" enctype="multipart/form-data">
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" maxlength="15" name="user"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="Password" name="password"></td>
+			</tr>
+			<tr>
+				<td colspan="3" align="center"><br><input type="submit" name="Kirim" value="Kirim"></td>
+			</tr>
+		</form>
+	</table>
+</body>
+</html>
