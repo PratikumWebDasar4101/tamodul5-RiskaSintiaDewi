42463b24749bc7f4bf5a04c92c6266a11b076e7b
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-10-01 08:30:11 -0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,14 @@
-<?php 
+<?php  
 
+$user=$_POST['user']? $_POST['user']:"Input tidak boleh kosong";
+$password=$_POST['password']? $_POST['password']:"Input tidak boleh kosong";
 
-
-
+if($user == "user" && $password == "123"){
+	$_SESSION["username"]=$username;
+	header("Location:registrasi.php");
+}else{
+	echo "Maaf Anda salah inputkan";
+}
 
 ?>
+<br><br>Kembali ke <a href="login.php">FORM</a>
